6544325c9f40c8109664e2bc30ed89b03b8fa657
rathi.snehal@gmail.com <rathi.snehal@gmail.com>
2023-11-10 20:26:02 +0530

    Updated Code

--- JenkinsDemo/src/test/java/Utility/BasicCalculator.java
+++ JenkinsDemo/src/test/java/Utility/BasicCalculator.java
@@ -4,23 +4,23 @@ import org.testng.annotations.Test;
 
 public class BasicCalculator {
 	
-	@Test
+	@Test(priority = 1)
 	public void sum()
 	{
 		int a=5;
 		int b=10;
 		int sum=a+b;
-		System.out.println("a = " + a + "\nb = " + b);
+	//	System.out.println("a = " + a + "\nb = " + b);
 		System.out.println("Sum = " + sum);	
 	}	
 	
-	@Test
+	@Test(priority = 2)
 	public void multiplication()
 	{
 		int a=5;
 		int b=10;
 		int result=a*b;
-		System.out.println("a = " + a + "\nb = " + b);
+	//	System.out.println("a = " + a + "\nb = " + b);
 		System.out.println("Multiplication = " + result);	
 	}
 
